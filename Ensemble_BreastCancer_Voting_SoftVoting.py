ab0ef343d7092211ae2ff1a2e8aaa147c8be3a6f
Omkar <omkarcodes6108@gmail.com>
2026-08-24 14:54:08 +0530

    Demonstraton of Ensemble ML Soft Voting on Breast Cancer case study

---
+++ Ensemble_BreastCancer_Voting_SoftVoting.py
@@ -0,0 +1,97 @@
+# this is multimodel 
+
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.ensemble import VotingClassifier
+
+#-------------------------------------
+# STEP 1 : Load the Dataset 
+#-------------------------------------
+
+df = pd.read_csv("breast_cancer.csv")
+
+print("Shape of Dataset : ", df.shape)
+print("First 5 Records : ")
+print(df.head())
+
+#----------------------------------------
+# STEP 2 : Seperate Features and Labels 
+#----------------------------------------
+
+X = df.drop("target", axis = 1)
+Y = df["target"]
+
+print("Shape of X : ", X.shape)
+print("Shape of Y : ", Y.shape)
+
+#--------------------------------------------------
+# STEP 3 : Split Dataset for Training and Testing
+#--------------------------------------------------
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
+
+
+#----------------------------------------
+# STEP 4 : Scale the Features 
+#----------------------------------------
+
+scalar = StandardScaler()
+
+X_train = scalar.fit_transform(X_train)
+X_test = scalar.fit_transform(X_test)
+# We don't scale Ytrain and Ytest because it contains 0 and 1 or such data generally which doesn't need to be scaled
+
+#----------------------------------------
+# STEP 5.1 : Create the Individual Models 
+#----------------------------------------
+
+model_logistic = LogisticRegression(max_iter = 1000)
+
+model_decisiontree = DecisionTreeClassifier(random_state = 42)
+
+model_knn = KNeighborsClassifier(n_neighbors = 5)
+
+#----------------------------------------
+# STEP 5.2 : Create the Voting Model 
+#----------------------------------------
+
+model = VotingClassifier(
+    estimators =[
+                    ['logistic',model_logistic],
+                    ['decision_tree',model_decisiontree],
+                    ['knn',model_knn]
+                ],
+
+    voting = 'soft'
+)
+
+#----------------------------------------
+# STEP 6 : Train the Model 
+#----------------------------------------
+
+model = model.fit(X_train, Y_train)
+
+#----------------------------------------
+# STEP 7 : Test the Model 
+#----------------------------------------
+
+Y_pred = model.predict(X_test)
+
+
+#----------------------------------------
+# STEP 8 : Evaluate the Model 
+#----------------------------------------
+
+print("Accuarcy Score is : ")
+print(accuracy_score(Y_test, Y_pred))
+
+print("Confusion Matrix : ")
+print(confusion_matrix(Y_test, Y_pred))
